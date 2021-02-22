2413b0505bd2dbb37507d76c55c38672a350c392
”Alexey <A_Goth@mail.ru>
2021-02-22 19:22:03 +0300

    begin

---
+++ app/src/main/java/ru/aslazarev/calc/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.aslazarev.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
